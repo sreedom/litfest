d9c746e68d2e0f7bda9437b97fa5bf774ff111bc
sreeraj.a <sreeraj.a@inmobi.com>
2016-12-09 23:05:46 +0530

    first commit

---
+++ 03-bouncing-letters.py
@@ -0,0 +1,119 @@
+# -*- coding: UTF-8 -*-
+import sys, random
+import pygame
+from pygame.locals import *
+import pymunk
+import pymunk.pygame_util
+
+import random
+#globals
+
+width, height = (600, 800)
+alphabets = u'ABCDEFGHIJKLMN'
+
+def get_random_letter():
+    return alphabets[random.randint(0, len(alphabets) - 1 )]
+
+def add_ball(space):
+    """Add a ball to the given space at a random position"""
+    mass = 1
+    radius = 14
+    inertia = pymunk.moment_for_circle(mass, 0, radius, (0,0))
+    body = pymunk.Body(mass, inertia)
+    x = random.randint(120,380)
+    body.position = x, 550
+    shape = pymunk.Circle(body, radius, (0,0))
+    shape.elasticity = 0.5
+    space.add(body, shape)
+    shape.letter = get_random_letter()
+    return shape
+
+def add_L(space):
+    """Add a inverted L shape with two joints"""
+    rotation_center_body = pymunk.Body(body_type = pymunk.Body.STATIC)
+    rotation_center_body.position = (300,300)
+
+    rotation_limit_body = pymunk.Body(body_type = pymunk.Body.STATIC)
+    rotation_limit_body.position = (200,300)
+
+    body = pymunk.Body(10, 10000)
+    body.position = (300,300)
+    l1 = pymunk.Segment(body, (-150, 0), (255.0, 0.0), 5.0)
+    l2 = pymunk.Segment(body, (-150.0, 0), (-150.0, 50.0), 5.0)
+
+    rotation_center_joint = pymunk.PinJoint(body, rotation_center_body, (0,0), (0,0))
+    joint_limit = 25
+    rotation_limit_joint = pymunk.SlideJoint(body, rotation_limit_body, (-100,0), (0,0), 0, joint_limit)
+
+    space.add(l1, l2, body, rotation_center_joint, rotation_limit_joint)
+    return l1,l2
+
+def add_boundaries(space):
+    "make the ground bouncy"
+    body = pymunk.Body(10, 10000, pymunk.Body.STATIC)
+    body.position = (0, 0)
+    ground = pymunk.Segment(body, (0, 0), (width,0), 5.0)
+    ground.elasticity = 0.6
+    space.add(ground)
+
+def draw_ball(screen, ball, font):
+    p = int(ball.body.position.x), height - int(ball.body.position.y)
+    #pygame.draw.circle(screen, (0,0,255), p, int(ball.radius), 2)
+    surface = font.render(ball.letter, 0, (255,0,0))
+    screen.blit(surface, (p[0] - ball.radius, p[1] - ball.radius))
+
+def main():
+    pygame.init()
+    font = pygame.font.Font(None, 48)
+    screen = pygame.display.set_mode((width, height))
+    pygame.display.set_caption("second")
+    clock = pygame.time.Clock()
+
+    space = pymunk.Space()
+    space.gravity = (0.0, -900.0)
+
+    #lines = add_L(space)
+    ground = add_boundaries(space)
+    balls = []
+    draw_options = pymunk.pygame_util.DrawOptions(screen)
+
+    ticks_to_next_ball = 10
+    n = 0
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                sys.exit(0)
+            elif event.type == KEYDOWN and event.key == K_ESCAPE:
+                sys.exit(0)
+
+        ticks_to_next_ball -= 1
+        if ticks_to_next_ball <= 0:
+            ticks_to_next_ball = 25
+            ball_shape = add_ball(space)
+            balls.append(ball_shape)
+            n+=1
+
+        screen.fill((255,255,255))
+
+        balls_to_remove = []
+        for ball in balls:
+            if ball.body.position.y < 0:
+                balls_to_remove.append(ball)
+
+        for ball in balls_to_remove:
+            space.remove(ball, ball.body)
+            balls.remove(ball)
+            n -= 1
+        pygame.display.set_caption(str(n) + " balls")
+
+        #space.debug_draw(draw_options)
+        for ball in balls:
+            draw_ball(screen, ball, font)
+
+        space.step(1/50.0)
+
+        pygame.display.flip()
+        clock.tick(50)
+
+if __name__ == '__main__':
+    main()
